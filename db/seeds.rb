3115f4d290a6c1bbdc0e3ef98325bbcfbd0f829b
Skyler Torian <skyler.torian@yahoo.com>
2020-03-10 15:15:09 -0400

    added three users

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,8 @@ Game.destroy_all
 GamePlatform.destroy_all
 Platform.destroy_all
 
+skyler = User.create(username:"skyler1", name:"Skyler", bio:"Just here to have some fun. I love playing Destiny 2", profile_picture:"nothing.jpg")
+jenny = User.create(username:"jenny1", name:"Jenny", bio:"I am a hard core gamer. I play Apex Legends!", profile_picture:"nothing.jpg")
+anna = User.create(username:"anna1", name:"Anna", bio:"I play Minecraft, that's it.", profile_picture:"nothing.jpg")
+
 
