3e599fefdfa2d0e22050da255562b2e1cd58aa88
Skyler Torian <skyler.torian@yahoo.com>
2020-03-10 15:12:01 -0400

    added destroy_all in seeds for reloading db to prevent duplicate records

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,11 @@ require 'rest-client'
 require 'json'
 require 'active-support/all'
 
+User.destroy_all
+UserFriend.destroy_all
+UserGame.destroy_all
+Game.destroy_all
+GamePlatform.destroy_all
+Platform.destroy_all
+
 
