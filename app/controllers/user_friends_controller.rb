63c0abe7f9e11af687d163167b5aa00cbfe0bf8c
Skyler Torian <skyler.torian@yahoo.com>
2020-03-24 16:15:40 -0400

    can add friends on back end

--- app/controllers/user_friends_controller.rb
+++ app/controllers/user_friends_controller.rb
@@ -3,4 +3,8 @@ class UserFriendsController < ApplicationController
         friends = UserFriend.all
         render json: friends
     end
+
+    def create
+        UserFriend.create(user_id: params[:user_id], friend_id: params[:friend_id], status: params[:status])
+    end
 end
