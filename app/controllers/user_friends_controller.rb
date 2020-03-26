1b9c5c6ba06e25f7aff2121bff7267fb99921189
Skyler Torian <skyler.torian@yahoo.com>
2020-03-26 00:00:08 -0400

    added validation to prevent duplication of friends

--- app/controllers/user_friends_controller.rb
+++ app/controllers/user_friends_controller.rb
@@ -1,12 +1,18 @@
 class UserFriendsController < ApplicationController
     def index
+        users = UserFriend.all
+        render json: users
+    end
+
+    def show
         user = User.find_by(id: params[:id])
         render json: user.friends
     end
-
+       
     def create
         UserFriend.create(user_id: params[:user_id], friend_id: params[:friend_id], status: params[:status])
     end
 
-
 end
+
+
--- app/models/user_friend.rb
+++ app/models/user_friend.rb
@@ -2,4 +2,6 @@ class UserFriend < ApplicationRecord
     belongs_to :user, class_name: 'User'
     belongs_to :friend, class_name: 'User'
 
+    validates :friend, uniqueness: true
+
 end
